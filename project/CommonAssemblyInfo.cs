326971ff80e985ec1942ded9e9955704dc4fca40
byron wall <byron@byroni.us>
2008-06-06 21:30:55 +0000

    Made some major changes related to the structure.  Everything is being setup for a build script.
    See #17

--- project/CommonAssemblyInfo.cs
+++ project/CommonAssemblyInfo.cs
@@ -7,14 +7,7 @@ using System.Windows;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("LastFMClient")]
-[assembly: AssemblyDescription("")]
-[assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("LastFMClient")]
-[assembly: AssemblyCopyright("Copyright ©  2008")]
-[assembly: AssemblyTrademark("")]
-[assembly: AssemblyCulture("")]
 
 // Setting ComVisible to false makes the types in this assembly not visible 
 // to COM components.  If you need to access a type in this assembly from 
