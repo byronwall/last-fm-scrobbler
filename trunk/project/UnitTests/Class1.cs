2211af9e9a1f3aad4e0e5121dfd6296a3c0d6214
byron <byron@aa07519c-f0cf-7f40-bc47-4cc95d8368da>
2008-06-07 03:26:18 +0000

    Renamed the unit test folder.

---
+++ trunk/project/UnitTests/Class1.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NUnit.Framework;
+
+namespace UnitTests
+{
+    [TestFixture]
+    public class Class1
+    {
+        [SetUp]
+        public void Setup()
+        {
+
+        }
+        [Test]
+        public void TrueTest()
+        {
+            Assert.IsTrue(true);
+
+        }
+        [TearDown]
+        public void TearDown()
+        {
+        }
+    }
+}
