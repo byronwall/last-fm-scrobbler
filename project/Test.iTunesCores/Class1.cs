f07c0282d4ea2a206e6555c282789a0f8e660f0f
byron wall <byron@byroni.us>
2008-06-07 01:43:39 +0000

    Update the unit tests.

--- project/Test.iTunesCores/Class1.cs
+++ project/Test.iTunesCores/Class1.cs
@@ -2,10 +2,27 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using NUnit.Framework;
 
 namespace UnitTests
 {
+    [TestFixture]
     public class Class1
     {
+        [SetUp]
+        public void Setup()
+        {
+
+        }
+        [Test]
+        public void TrueTest()
+        {
+            Assert.IsTrue(true);
+
+        }
+        [TearDown]
+        public void TearDown()
+        {
+        }
     }
 }
