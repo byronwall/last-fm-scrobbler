f28442814605c416baaa5a5e5aac27075e11453f
byron wall <byron@byroni.us>
2008-06-10 16:01:35 +0000

    Includes the updated unit testing files.  Also rearranged the class around to a better folder structure.
    Unit tests included:
    TestiTunesDatabase
    TestiTunesDatabaseWithFile

---
+++ project/UnitTests/DatabaseTests/TestiTunesDatabaseWithFile.cs
@@ -0,0 +1,36 @@
+﻿using iTunesCore;
+using NUnit.Framework;
+
+namespace UnitTests
+{
+    [TestFixture]
+    public class TestiTunesDatabaseWithFile
+    {
+        private const string filename = @"F:\My Documents\My Music\iTunes\iTunes Music Library.xml";
+        private readonly iTunesDatabase Database = new iTunesDatabase(new ITunesDatabaseProviderFromFile(filename));
+
+        [SetUp]
+        public void SetupTest()
+        {
+
+        }
+        [Test]
+        public void InitialObject()
+        {
+            Assert.AreEqual(Database.State, DatabaseState.Unloaded);
+
+        }
+
+        [Test]
+        public void HasElements()
+        {
+            Database.LoadDatabase();
+            Assert.Greater(Database.Tracks.Count, 0);
+        }
+        [TearDown]
+        public void TearDownTest()
+        {
+        }
+
+    }
+}
